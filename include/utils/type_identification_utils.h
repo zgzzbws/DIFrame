21bdf0ce234dab850b2d770c85f6918c0705bbe2
zgzzbws <song.bowen@outlook.com>
2017-04-01 17:41:59 +0800

    log message

--- include/utils/type_identification_utils.h
+++ include/utils/type_identification_utils.h
@@ -1,2 +1,12 @@
-#ifndef
-#define
+#ifndef DIFRAME_TYPE_IDENTIFICATION_UTILS_H
+#define DIFRAME_TYPE_IDENTIFICATION_UTILS_H
+
+#include <memory>
+
+namespace DIFrame {
+    namespace utils {
+        
+    }
+}
+
+#endif
