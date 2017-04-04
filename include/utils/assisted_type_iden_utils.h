99e6114f111c8c664e193e103d879febebe37352
zgzzbws <song.bowen@outlook.com>
2017-04-04 22:09:32 +0800

    The head file that supports the assisted part

---
+++ include/utils/assisted_type_iden_utils.h
@@ -0,0 +1,14 @@
+#ifndef DIFRAME_ASSISTED_IDEN_UTILS_H
+#define DIFRAME_ASSISTED_IDEN_UTILS_H
+
+#include "type_identification_utils.h"
+
+namespace DIFrame {
+
+    namespace utils {
+
+    }
+
+}  //DIFrame
+
+#endif
