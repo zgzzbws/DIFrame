5585e019eb59a1878f62ea3f6a5e931437a816f8
zgzzbws <song.bowen@outlook.com>
2017-04-08 00:16:46 +0800

    new tools add to the utils

--- include/utils/parameter_passing_utils.h
+++ include/utils/parameter_passing_utils.h
@@ -10,6 +10,7 @@ namespace DIFrame {
         template <typename... parameters>
         struct parm { };
         
+        //********************
         //To judge whether the parameter list has one or multi-parameters
         //If there is just one parameter in the parameter list
         template <typename T>
@@ -19,9 +20,97 @@ namespace DIFrame {
         template <typename... Ts>
         struct has_multi_parameters <parm<Ts...>> : public std::true_type {};
 
+        //********************
+        //To attach a parameter to the parameter list
+        template <typename T, typename P>
+        struct attach_parm_impl {};
+
+        template <typename T, typename... Ts>
+        struct attach_parm_impl <T, parm<Ts...>> {
+            using type = parm<T, Ts...>;
+        };
+
+        template <typename T, typename  P>
+        using attach_parm = typename attach_parm_impl<T, P>::type;
+
+        //********************
+        //To join new parameter lists to the existing one
+        template <typename... P>
+        struct join_parm_impl {};
+
+        template <>
+        struct join_parm_impl <> {
+            using type = parm <>;
+        };
+
+        template <typename P>
+        struct join_parm_impl <P> {
+            using type = P;
+        };
+
+       template <typename... Ts, typename... Us, typename... Ps>
+       struct join_parm_impl <parm<Ts...>, parm<Us...>, Ps...> {
+           using type = typename join_parm_impl<parm<Ts..., Us...>, Ps...>::type;
+       };
+ 
+       template <typename... Ps>
+       using join_parm = typename join_parm_impl<Ps...>::type;
+
+       //********************
+       //To remove the existing parameter from parameter list
+       template <typename T, typename P>
+       struct remove_parm_impl {};
+
+       template <typename T, typename... Ps>
+       struct remove_parm_impl <T, parm<Ps...>> {
+           using type = join_parm <
+               typename std::conditional<std::is_same<T, Ps>::value, parm<>, parm<Ps>>::type
+           ...>;
+       };
+
+       template <typename T, typename P>
+       using remove_parm = typename remove_parm_impl<T, P>::type;
+      
+       //********************
+       //To judge whether the parameter is in the list
+       template<typename T, typename P>
+       struct parm_in_list {};
+
+       template <typename T, typename... Ts>
+       struct parm_in_list <T, parm<Ts...>> : public static_or<std::is_same<T, Ts>::value...> {}; 
+        
+       //********************
+       //If the parameter list is empty
+       template<typename P>
+       struct empty_parm_list : public std::is_same<parm<>, P> {};
+
+       //********************
+       //If you want to add the parameter to the list using this instead of calling join_parm_impl directly
+       //This struct can attach the parameters according to the proper condition
+       template <bool is_in_list, typename T, typename P>
+       struct add_to_parm_impl {};
+
+       template <typename T, typename P>
+       struct add_to_parm_impl <false, T, P> {
+           using type = join_parm <T, P>;
+       };
+
+       template <typename T, typename P>
+       struct add_to_parm_impl <true, T, P> {
+           using type = P
+       };
+
+       template <typename T, typename P>
+       using add_to_parm = typename add_to_parm_impl<parm_in_list<T, P>::value, T, P>::type;
+
+       //********************
+       //To judge whether there is intersection between two parameter lists
         
 
-    }
-}
+
+     
+
+    } //namespace utils
+} //namespace DIFrame
 
 #endif
