2dc40031501a26a8ec3deacf9077647de9e3a422
zgzzbws <song.bowen@outlook.com>
2017-04-09 00:05:44 +0800

    some new structs

--- include/utils/parameter_passing_utils.h
+++ include/utils/parameter_passing_utils.h
@@ -104,10 +104,100 @@ namespace DIFrame {
        using add_to_parm = typename add_to_parm_impl<parm_in_list<T, P>::value, T, P>::type;
 
        //********************
-       //To judge whether there is intersection between two parameter lists
+       //To return the common part if there is intersection between two parameter lists
+       template <typename P1, typename P2>
+       struct parm_intersection_impl {};
+
+       template <typename... Ts, typename... Us>
+       struct parm_intersection_impl <parm<Ts...>, parm<Us...>> {
+           using type = join_parm<
+               typename std::conditional<parm_in_list<Ts, parm<Us...>>::value, parm<Ts>, parm<>>::type;
+           ...>;
+       };
+
+       template <typename T, typename... Ts, typename... Us>
+       struct parm_intersection_impl <parm<T, Ts...>, parm<Us...>> {
+           using Ts_in_Us = typename parm_intersection_impl <parm<Ts...>, parm<Us...>>::type;
+           using type = typename std::conditional <
+               parm_in_list <T, parm<Us...>>::value,
+               join_parm <T, Ts_in_Us>,
+               Ts_in_Us >::type;
+
+       };
+
+       template <typename P1, typename P2>
+       using parm_intersection = typename parm_intersection_impl<P1, P2>::type;
+     
+       //********************
+       //The function to combine two parameter lists
+       template <typename P1, typename P2>
+       struct combine_parm_lists_impl {};
+
+       template <typename P>
+       struct combine_parm_lists_impl <parm<>, P> {
+           using type = P;
+       };
+
+       template <typename T1, typename... Ts, typename P>
+       struct combine_parm_lists_impl <parm<T1, Ts...>, P> {
+           using temp_merge = typename combine_parm_lists_impl<parm<Ts...>, P>::type;
+           using type = typename add_to_parm<T1, temp_merge>;
+       };
+
+       template <typename P1, typename P2>
+       using combine_parm_lists = typename combine_parm_lists_impl <P1, P2>::type;
         
+       //********************
+       //To replace the specific parameters in the parameter list
+       template <bool T_in_P, typename T, typename P1, typename P>
+       struct replace_parm_impl {};
 
+       template <typename T, typename P1, typename P>
+       struct repace_parm_impl <false, T, P1, P> {
+           using type = P;
+       };
+   
+       template <typename T, typename P1, typename P>
+       struct replace_parm_impl <true, T, P1, P>  {
+           using type = combine_parm_lists<P1, remove_parm<T, P>>;
+       };
 
+       template <bool T_in_P, typename T, typename P1, typename P>
+       using replace_parm = typename replace_parm_impl<parm_in_list<T, P>::value, T, P1, P>::type;
+
+       //********************
+       //To remove duplicate elements in the list
+      template <typename P>
+      struct remove_duplicate_element_impl {
+          using type = parm<>;
+      };
+
+      template <typename T1, typename...T>
+      struct remove_duplicate_element_impl <parm<T1, T...>> {
+          using remove_duplicate_temp = typename remove_duplicate_element_impl<parm<T...>>::type;
+          using type = add_to_parm<T1, remove_duplicate_temp>; 
+      };
+
+      template <typename P>
+      using remove_duplicate_element = typename remove_duplicate_element_impl<P>::type;
+
+      //********************
+      //To figure out the difference between two parameter lists
+      template <typename P1, typename P2>
+      struct figure_differences_impl {};
+
+      template <typename... Ts, typename... Us>
+      struct figure_differences_impl <parm<Ts...>, parm<Us...>> {
+          using type = join_parm <
+              typename std::conditional<parm_in_list<Ts, parm<Us...>>::value, parm<>, parm<Ts>>::type
+          ...>;
+      };
+
+     template <typename P1, typename P2>
+     using figure_differences = typename figure_differences_impl<P1, P2>::type;
+
+     //********************
+     
      
 
     } //namespace utils
