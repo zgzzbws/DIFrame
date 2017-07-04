935e20e6e5a95c2346b90abc895d770674023721
Bowen Song <song.bowen@outlook.com>
2017-07-04 16:59:57 +0800

    The main head file
    This head file should be included when using this frame

--- include/DIFrame.h
+++ include/DIFrame.h
@@ -1,7 +1,9 @@
 #ifndef DIFRAME_DIFRAME_H
 #define DIFRAME_DIFRAME_H
 
+#include "macro.h"
+#include "forward_declaration.h"
 #include "module.h"
-#include "injector.h"
+#include "getter.h"
 
 #endif // DIFRAME_DIFRAME_H
