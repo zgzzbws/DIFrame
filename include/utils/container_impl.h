1642fb17b598ec746bf4dc8a5dd46d0864f705d6
Bowen Song <song.bowen@outlook.com>
2017-07-08 19:01:44 +0800

    The implement of container impl

--- include/utils/container_impl.h
+++ include/utils/container_impl.h
@@ -1,6 +1,27 @@
 #ifndef DIFRAME_CONTAINER_IMPL_H
 #define DIFRAME_CONTAINER_IMPL_H
 
+namespace DIFrame {
+namespace utils {
+    
+    template <typename Dependency, typename Constructor, typename Signature, typename Settings>
+    class ContainerImpl {
+    private:
+        Build build;
 
+    public:
+        ContainerImpl() = default;
+        ContainerImpl( ContainerImpl&& containerImpl );
+        
+        template <typename Dependency_Other, typename Constructor_Other, typename Signature_Other, typename Settings_Other>
+        ContainerImpl( const ContainerImpl<Dependency_Other, Constructor_Other, Signature_Other, Settings_Other>& ContainerImpl_Other );
 
-#endif
+        
+
+        
+    };
+
+} // namespace utils
+} //namespace DIFrame
+
+#endif // DIFRAME_CONTAINER_IMPL_H
