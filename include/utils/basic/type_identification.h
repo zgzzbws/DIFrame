d1f267743000020965a2451eb0bbdeb5980e706c
Bowen Song <song.bowen@outlook.com>
2017-07-11 16:59:04 +0800

    ReConstruct

---
+++ include/utils/basic/type_identification.h
@@ -0,0 +1,27 @@
+#ifndef DIFRAME_TYPE_IDENTIFICATION_H
+#define DIFRAME_TYPE_IDENTIFICATION_H
+
+namespace DIFrame {
+namespace utils{
+
+    template <typename T>
+    struct getTypeHelper {
+        using type = T;
+    };
+
+    template <typename T>
+    struct getTypeHelper <const T> {
+        using type = T;
+    }
+
+    template <typename T>
+    struct getTypeHelper <T&> {
+        using type = T;
+    }
+
+    
+
+} // namespace utils
+} //namespace DIFrame
+
+#endif // DIFRAME_TYPE_IDENTIFICATION_H
--- include/utils/container_impl.h
+++ include/utils/container_impl.h
@@ -9,14 +9,32 @@ namespace utils {
     private:
         Build build;
 
-    public:
+    public: 
         ContainerImpl() = default;
         ContainerImpl( ContainerImpl&& containerImpl );
         
         template <typename Dependency_Other, typename Constructor_Other, typename Signature_Other, typename Settings_Other>
         ContainerImpl( const ContainerImpl<Dependency_Other, Constructor_Other, Signature_Other, Settings_Other>& ContainerImpl_Other );
 
+    public:
+        using Dep = Dependency;
+        using Cons = Constructor;
+        using Sig = Signature;
+        using Set = Settings;
+        using This = PartialComponent<Dep, Cons, Sig, Set>;
+
+    public:
+        //The friend class or structs to this container
+        template <typename... Types>
+        friend class DIFrame::Get;
+
+        template <typename... Types>
+        friend class DIFrame::Container;
+
         
+
+
+
 
         
     };
