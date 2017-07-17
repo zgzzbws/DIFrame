bf01f81a41ed96915748d973449079131514f854
Bowen Song <song.bowen@outlook.com>
2017-07-17 10:00:52 +0800

    The implement of Reflections

--- src/reflections.cpp
+++ src/reflections.cpp
@@ -13,15 +13,38 @@ namespace utils{
     }
 
     Reflections& Reflections::operator=( Reflections&& other ) {
-        
+
     }
 
     void Reflections::createDynamicBuild( TypeIndex typeIndex, void* ParamsForCreate, void* (*create)(Reflections&, void*), void (*destroy)(void*) ) {
-
+        auto findResult = DynamicBuildMap.find( typeIndex );
+        if( findResult == DynamicBuildMap.end() ) {
+            DynamicBuild& newBuild = DynamicBuildMap[typeIndex];
+            newBuild.Instance = nullptr;
+            newBuild.ParamsForCreate = ParamsForCreate;
+            newBuild.create = create;
+            newBuild.destroy = destroy;
+        } else {
+            DynamicBuild& foundBuild = findResult->second;
+            bool judgement = (foundBuild.ParamsForCreate == ParamsForCreate) &&
+                             (foundBuild.create == create) &&
+                             (foundBuild.destroy == destroy);
+            //check( judgement );
+        }
     }
 
     void Reflections::createDynamicBuild( TypeIndex typeIndex, void* Instance, void (*destroy)(void*) ) {
-
+        auto findResult = DynamicBuildMap.find( typeIndex );
+        if( findResult == DynamicBuildMap.end() ) {
+            DynamicBuild& newBuild = DynamicBuildMap[ typeIndex ];
+            newBuild.Instance = Instance;
+            newBuild.destroy = destroy;
+        } else {
+            DynamicBuild& foundBuild = findResult->second;
+            bool judgement = (foundBuild.Instance == Instance) &&
+                             (foundBuild.destroy == destroy);
+            //check( judgement );
+        }
     }
 
     void* Reflections::getInstanceHelper( TypeIndex typeIndex ) {
