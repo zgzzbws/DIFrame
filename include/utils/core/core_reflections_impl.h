f81c8437fbe8b9f1a2c19b593f083c85e4e70331
Bowen Song <song.bowen@outlook.com>
2017-07-19 15:13:21 +0800

    To finish the part of setwithdependency

--- include/utils/core/core_reflections_impl.h
+++ include/utils/core/core_reflections_impl.h
@@ -7,8 +7,8 @@ namespace utils {
     template <typename Interface, typename Implement>
     inline void set() {
         //TODO: guarantee Interface and Implement are pointers
-        static_assert(!std::is_pointer<Interface>::value, "The Interface cannot be a pointer");
-        static_assert(!std::is_pointer<Implement>::value, "The Implement cannot be a pointer");
+        static_assert( !std::is_pointer<Interface>::value, "DIFrame Error : The Interface cannot be a pointer" );
+        static_assert( !std::is_pointer<Implement>::value, "DIFrame Error : The Implement cannot be a pointer" );
         auto create = []( Reflections& reflections, void* ) {
             Implement* ImplementPtr = reflections.getInstanceHelper<Implement>();
             Interface* InterfacePtr = static_cast<Interface*>(ImplementPtr);
@@ -25,12 +25,29 @@ namespace utils {
 
     template <typename Implement, typename... Dependencies>
     inline void setWithDependency( Implement* (*funptr)(Dependencies...), void (*deleteptr)(void*) ) {
-
+        static_assert( !std::is_pointer<Implement>::value, "DIFrame Error : The Implement cannot be a pointer" );
+        //check( funptr != nullptr );
+        using funType = decltype( funptr );
+        auto create = []( Reflections& reflections, void* deps ) {
+            funType funptr = reinterpret_cast<funType>(deps);
+            Implement* ImplementPtr = funptr( reflections.fetch<Dependencies>()... );
+            return reinterpret_cast<void*>(ImplementPtr);
+        };
+        createDynamicBuild<Implement>(reinterpret_cast<void*>(funptr), create, deleteptr);
     }
 
     template <typename Implement, typename... Dependencies>
     inline void setWithDependency( Implement (*funptr)(Dependencies...), void (*deleteptr)(void*) ) {
-        
+        static_assert( !std::is_pointer<Implement>::value, "DIFrame Error : The Implement cannot be a pointer" );
+        static_assert(std::is_move_constructible<Implement>::value, "DIFrame Error : The Implement should be movable");
+        //check( funptr != nullptr );
+        using funType = decltype( funptr );
+        auto create = []( Reflections& reflections, void* deps ) {
+            funType funptr = reinterpret_cast<funType>(deps);
+            Implement* ImplementPtr = new Implement(funptr( reflections.fetch<Dependencies>()... ));
+            return reinterpret_cast<void*>(ImplementPtr);
+        };
+        createDynamicBuild<Implement>(reinterpret_cast<void*>(funptr), create, deleteptr);
     }
 
 
